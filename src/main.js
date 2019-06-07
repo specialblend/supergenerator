a98dc3b626d874fcc029cc2f4e579156a0d8b054
specialblend <cj@specialblend.net>
2019-06-06 20:22:32 -0500

    fix console

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
-require('./console');
-
 const Generator = require('yeoman-generator');
 const R = require('ramda');
 const chalk = require('chalk');
 const yosay = require('yosay');
+
+const { info, warn, error, debug } = console;
+
+const red = '#990000';
+const orange = '#cc7832';
+const darkGreen = '#8c8c34';
+const lightGreen = '#ccc147';
+const gray = '#8598a6';
+
+console.info = R.compose(info, chalk.hex(darkGreen));
+console.success = R.compose(info, chalk.hex(lightGreen));
+console.warn = R.compose(warn, chalk.hex(orange));
+console.error = R.compose(error, chalk.bgHex(red).white);
+console.debug = R.compose(debug, chalk.hex(gray));
 
 const __initializeSetup__ = Symbol('__initializeSetup__');
 const __setupPackageJSON__ = Symbol('__setupPackageJSON__');
